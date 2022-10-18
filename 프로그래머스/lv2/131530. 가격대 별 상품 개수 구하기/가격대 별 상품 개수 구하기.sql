1cf584b0d09354c2c579c72d98d3d1f04398d10c
wonil <80016735+wonillin@users.noreply.github.com>
2022-10-18 11:14:33 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,27 +1,6 @@
 -- 코드를 입력하세요
-select 
-    case
-        when price between 0 and 9999 then '0'
-        when price between 10000 and 19999 then '10000'
-        when price between 20000 and 29999 then '20000'
-        when price between 30000 and 39999 then '30000'
-        when price between 40000 and 49999 then '40000'
-        when price between 50000 and 59999 then '50000'
-        when price between 60000 and 69999 then '60000'
-        when price between 70000 and 79999 then '70000'
-        when price between 80000 and 89999 then '80000'
-    end price_group,
-    case
-        when price between 0 and 9999 then count(product_id)
-        when price between 10000 and 19999 then count(product_id)
-        when price between 20000 and 29999 then count(product_id)
-        when price between 30000 and 39999 then count(product_id)
-        when price between 40000 and 49999 then count(product_id)
-        when price between 50000 and 59999 then count(product_id)
-        when price between 60000 and 69999 then count(product_id)
-        when price between 70000 and 79999 then count(product_id)
-        when price between 80000 and 89999 then count(product_id)
-    end products
+select truncate(price, -4) price_group
+      ,count(product_id) products
 from product
-group by 1
-order by 1
+group by truncate(price, -4)
+order by truncate(price, -4)
