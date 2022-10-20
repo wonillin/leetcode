c43e9851c24494e7d8142dc53cf55e55049c8a2a
wonil <80016735+wonillin@users.noreply.github.com>
2022-10-20 18:23:00 +0900

    Delete 프로그래머스/unrated/132202. 진료과별 총 예약 횟수 출력하기 directory

--- 프로그래머스/unrated/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++
@@ -1,7 +0,0 @@
--- 코드를 입력하세요
-SELECT mcdp_cd '진료과코드'
-      ,count(mcdp_cd) '5월예약건수'
-from appointment
-where apnt_ymd between '2022-05-01 00:00:00' and '2022-05-31 23:59:59'
-group by mcdp_cd
-order by count(mcdp_cd), mcdp_cd
