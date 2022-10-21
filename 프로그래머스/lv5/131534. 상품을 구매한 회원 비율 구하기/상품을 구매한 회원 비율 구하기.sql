419768e2150cc5dae61149ae7ccc7063eeb5ff31
wonil <80016735+wonillin@users.noreply.github.com>
2022-10-21 11:56:45 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,11 +1,11 @@
 -- 코드를 입력하세요
-select year(sales_date) 'year'
-      ,month(sales_date) 'month'
-      ,count(distinct os.user_id) puchased_user
-      ,round(count(distinct os.user_id)/(select count(*) from user_info
-                                          where year(joined) = '2021'), 1) puchased_ratio
-from user_info ui
-     left join online_sale os on ui.user_id = os.user_id
-where joined between '2021-01-01 00:00:00' and '2021-12-31 23:59:59'
-      and year(sales_date) is not null
-group by year(sales_date), month(sales_date)
+SELECT YEAR(sales_date) 'year'
+      ,MONTH(sales_date) 'month'
+      ,COUNT(DISTINCT os.user_id) puchased_user
+      ,ROUND(COUNT(DISTINCT os.user_id)/(SELECT COUNT(*) FROM user_info
+                                          WHERE YEAR(joined) = '2021'), 1) puchased_ratio
+  FROM user_info ui
+       LEFT JOIN online_sale os ON ui.user_id = os.user_id
+WHERE joined BETWEEN '2021-01-01 00:00:00' AND '2021-12-31 23:59:59'
+  AND year(sales_date) IS NOT NULL
+GROUP BY YEAR(sales_date), MONTH(sales_date)
