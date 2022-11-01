e45e9bbe19282c4d4a26782fc377f83eb4ef67bd
wonil <80016735+wonillin@users.noreply.github.com>
2022-11-01 12:30:28 +0900

    Time: 372 ms (53.15%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,15 +1,14 @@
-# Write your MySQL query statement below
 # Write your MySQL query statement below\
-with cte as
-(select id, student
-      ,lag(student, 1) over (order by id) a
-      ,lead(student, 1) over (order by id) b
-from seat)
 
-select id,
-    case
-        when id%2 = 0 then a 
-        when id%2 != 0 and b is null then student
-        else b
-    end student
-from cte
+SELECT id
+      ,CASE 
+          WHEN id % 2 = 0 THEN a.before
+          WHEN id % 2 != 0 AND a.after IS NULL THEN student
+          ELSE a.after
+        END 'student'
+  FROM
+     (SELECT * 
+          ,LEAD(student) OVER (ORDER BY id) 'after'
+          ,LAG(student) OVER (ORDER BY id) 'before' 
+       FROM seat) a
+
