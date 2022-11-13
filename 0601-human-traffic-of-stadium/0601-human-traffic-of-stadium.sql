39ccdeb4824350c99e5d2711b94c20669781963f
wonil <80016735+wonillin@users.noreply.github.com>
2022-11-14 01:33:19 +0900

    Time: 723 ms (10.88%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,15 +1,17 @@
 # Write your MySQL query statement below
-with cte as
-(select id
-      ,row_number() over (order by id) rn 
+WITH cte AS(
+SELECT id
+      ,ROW_NUMBER() OVER (ORDER BY id) rn 
       ,visit_date
       ,people
-from stadium
-where people >= 100)
+  FROM stadium
+ WHERE people >= 100)
 
-select id, visit_date, people
-from cte
-where id - rn in (select (id - rn) 
-                  from cte
-                  group by (id - rn)
-                  having count(*) >= 3)
+SELECT id
+      ,visit_date
+      ,people
+  FROM cte
+ WHERE id - rn IN (SELECT (id - rn)
+                     FROM cte
+                    GROUP BY (id - rn)
+                    HAVING COUNT(*) >= 3)
