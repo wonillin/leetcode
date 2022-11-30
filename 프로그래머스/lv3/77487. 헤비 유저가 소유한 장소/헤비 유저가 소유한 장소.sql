53789e1fd0dda104fa24a38c9df06da10b88ba42
wonil <80016735+wonillin@users.noreply.github.com>
2022-11-30 12:02:29 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -2,9 +2,9 @@
 SELECT id
       ,name
       ,host_id
-FROM places
-WHERE host_id not in (SELECT host_id
-                      FROM places
-                      GROUP BY host_id
-                      HAVING COUNT(host_id) <= 1)
-ORDER BY id
+  FROM places
+ WHERE host_id IN (SELECT host_id
+                     FROM places
+                    GROUP BY host_id
+                    HAVING COUNT(host_id) > 1)
+    
