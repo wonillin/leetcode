5ed682bc74e83fe749621e84bb427da21d9fc4ec
wonil <80016735+wonillin@users.noreply.github.com>
2022-12-23 11:38:20 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,9 @@
 -- 코드를 입력하세요
-SELECT ao.animal_id animal_id
+SELECT ao.animal_id
       ,ao.name
-  FROM animal_ins ai
-       RIGHT JOIN animal_outs ao ON ai.animal_id = ao.animal_id
-WHERE ai.animal_id IS NULL
-ORDER BY animal_id
+  FROM animal_outs ao 
+       LEFT JOIN animal_ins ai ON ao.animal_id = ai.animal_id
+ WHERE ai.animal_id IS NULL
+ ORDER BY animal_id
+      
+  
