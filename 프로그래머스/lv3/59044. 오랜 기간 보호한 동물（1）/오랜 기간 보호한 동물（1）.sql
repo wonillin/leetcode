3e14942cd805ae6098cc7ecaac57afc412d5df0d
wonil <80016735+wonillin@users.noreply.github.com>
2022-12-23 11:52:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -3,6 +3,6 @@ SELECT ai.name
       ,ai.datetime
   FROM animal_ins ai
        LEFT JOIN animal_outs ao ON ai.animal_id = ao.animal_id
-WHERE ao.animal_id IS NULL
-ORDER BY datetime 
-LIMIT 3
+ WHERE ao.animal_id IS NULL
+ ORDER BY datetime
+ LIMIT 3
