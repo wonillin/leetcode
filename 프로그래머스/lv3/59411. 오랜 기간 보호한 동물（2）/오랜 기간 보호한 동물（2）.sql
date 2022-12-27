362ac42ba618e7fa2ef9eed4dca7b608b0fbc84f
wonil <80016735+wonillin@users.noreply.github.com>
2022-12-27 11:09:32 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -3,5 +3,5 @@ SELECT ai.animal_id
       ,ai.name
   FROM animal_ins ai
        INNER JOIN animal_outs ao ON ai.animal_id = ao.animal_id
- ORDER BY TIMESTAMPDIFF(second, ai.datetime, ao.datetime) DESC
+ ORDER BY TIMESTAMPDIFF(SECOND, ai.datetime, ao.datetime) DESC
  LIMIT 2
