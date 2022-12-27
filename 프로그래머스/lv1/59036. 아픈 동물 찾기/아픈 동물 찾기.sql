936a520ecaa48ab4bcbbecfc7cfbc22cb5abfb29
wonil <80016735+wonillin@users.noreply.github.com>
2022-12-27 10:57:52 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID
-      ,NAME
-  FROM ANIMAL_INS
- WHERE INTAKE_CONDITION LIKE 'Sick'
- ORDER BY ANIMAL_ID
+SELECT animal_id
+      ,name
+  FROM animal_ins
+ WHERE intake_condition REGEXP 'sick'
+ ORDER BY animal_id
