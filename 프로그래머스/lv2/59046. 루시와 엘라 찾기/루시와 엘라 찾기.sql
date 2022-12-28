3f3808e4cfd7cc52c8cc6915df6995bb36d19d30
wonil <80016735+wonillin@users.noreply.github.com>
2022-12-28 14:16:54 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -3,4 +3,4 @@ SELECT animal_id
       ,name
       ,sex_upon_intake
   FROM animal_ins
-WHERE name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ WHERE name IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
