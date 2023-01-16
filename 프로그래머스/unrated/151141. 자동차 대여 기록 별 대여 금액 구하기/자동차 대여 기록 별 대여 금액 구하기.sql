dc93b43e113b741654f3098495d153fe81e6280f
wonil <80016735+wonillin@users.noreply.github.com>
2023-01-16 22:42:00 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,19 +1,31 @@
-WITH cte AS
-(SELECT history_id
-       ,daily_fee
-       ,(discount_rate * 0.01) discount_rate
-       ,REGEXP_REPLACE(duration_type, '[가-힣]','') duration_type
-       ,DATEDIFF(end_date, start_date) + 1 rent_day
-  FROM car_rental_company_car cc
-       INNER JOIN car_rental_company_rental_history cr ON cc.car_id = cr.car_id
-       INNER JOIN car_rental_company_discount_plan cdp on cc.car_type = cdp.car_type
- WHERE cc.car_type = '트럭')
+# SELECT DISTINCT history_id
+#       ,daily_fee * (DATEDIFF(end_date, start_date) + 1) , (IFNULL((SELECT REPLACE(DISCOUNT_RATE, '%', '')
+#                                                                         FROM car_rental_company_discount_plan
+#                                                                        WHERE CAR_TYPE = '트럭' AND DATEDIFF(END_DATE, START_DATE) + 1 >= REGEXP_REPLACE(duration_type, '[가-힣]','')
+#                                                                        ORDER BY duration_type DESC
+#                                                                        LIMIT 1
+#                                                                        ), 0) * 0.01) fee
+#  FROM car_rental_company_car cc
+#       INNER JOIN car_rental_company_rental_history cr ON cc.car_id = cr.car_id
+# WHERE cc.car_type = '트럭'
+# ORDER BY fee DESC, history_id DESC
+# SELECT REGEXP_REPLACE(duration_type, '[가-힣]','') duration_type
+# FROM car_rental_company_car cc
+#      INNER JOIN car_rental_company_rental_history cr ON cc.car_id = cr.car_id
+#      INNER JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN cd ON cc.car_type = cd.car_type
+# WHERE cc.CAR_TYPE = '트럭' AND DATEDIFF(END_DATE, START_DATE) + 1 >= REGEXP_REPLACE(duration_type, '[가-힣]','')
+# ORDER BY duration_type DESC
 
-SELECT DISTINCT history_id
-      ,CASE WHEN rent_day < 7 THEN ROUND(daily_fee * rent_day)
-            WHEN rent_day < 30 THEN ROUND(daily_fee * rent_day * (1 - 0.05))
-            WHEN rent_day < 90 THEN ROUND(daily_fee * rent_day * (1 - 0.08))
-            ELSE ROUND(daily_fee * rent_day * (1 - 0.15))
-            END fee
- FROM cte
- ORDER BY fee DESC, history_id DESC
+
+SELECT HISTORY_ID, ROUND((DATEDIFF(END_DATE, START_DATE) + 1) * DAILY_FEE * (1 - (
+    IFNULL((SELECT REPLACE(DISCOUNT_RATE, '%', '')
+        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+        WHERE CAR_TYPE = CC.CAR_TYPE AND DATEDIFF(END_DATE, START_DATE) + 1 >= REPLACE(duration_type, '일 이상', '')
+        ORDER BY REPLACE(DISCOUNT_RATE, '%', '') DESC
+        LIMIT 1
+    ), 0) * .01)), 0) FEE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+INNER JOIN CAR_RENTAL_COMPANY_CAR CC
+USING(CAR_ID)
+WHERE CAR_TYPE = '트럭'
+ORDER BY FEE DESC, HISTORY_ID DESC;
