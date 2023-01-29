73489c1a9ebe7368fdd8a0d31e9e7a106f0b55ed
wonil <80016735+wonillin@users.noreply.github.com>
2023-01-29 23:30:07 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -4,10 +4,7 @@ SELECT food_type
       ,rest_name
       ,favorites
   FROM rest_info
- WHERE rest_name IN (SELECT rest_name
-                       FROM (SELECT rest_name
-                                   ,RANK() OVER(PARTITION BY food_type ORDER BY favorites DESC) rn
-                               FROM rest_info) a
-                      WHERE rn =1 )
+ WHERE (food_type, favorites) IN (SELECT food_type
+                                        ,MAX(favorites) OVER(PARTITION BY food_type)
+                                    FROM rest_info)
  ORDER BY food_type DESC
- 
