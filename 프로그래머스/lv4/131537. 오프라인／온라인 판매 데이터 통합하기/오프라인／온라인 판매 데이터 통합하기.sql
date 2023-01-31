6ac989fd92c3f23c00ff188a26c9fad01a20c746
wonil <80016735+wonillin@users.noreply.github.com>
2023-01-31 23:30:39 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -4,14 +4,15 @@ SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') sales_date
       ,user_id
       ,sales_amount
   FROM online_sale
- WHERE sales_date BETWEEN '2022-03-01 00:00:00' AND '2022-03-31 23:59:59'
+ WHERE sales_date BETWEEN '2022-03-01' AND '2022-03-31'
 
-UNION
+UNION ALL
 
 SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') sales_date
       ,product_id
       ,NULL AS user_id
       ,sales_amount
   FROM offline_sale
- WHERE sales_date BETWEEN '2022-03-01 00:00:00' AND '2022-03-31 23:59:59'
+ WHERE sales_date BETWEEN '2022-03-01' AND '2022-03-31'
+ GROUP BY sales_date, product_id, user_id
  ORDER BY sales_date, product_id, user_id
