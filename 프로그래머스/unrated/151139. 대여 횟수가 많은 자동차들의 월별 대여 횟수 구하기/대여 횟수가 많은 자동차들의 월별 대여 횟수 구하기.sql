8cfe6eea66ebc7f850b9275a28a3033bf6e71df5
wonil <80016735+wonillin@users.noreply.github.com>
2023-02-03 23:58:01 +0900

    [unrated] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,12 +1,26 @@
 SELECT month
-     ,car_id
-     ,records
-  FROM 
-(SELECT DISTINCT MONTH(START_DATE) AS MONTH
-        , CAR_ID
-        , COUNT(*) OVER(PARTITION BY MONTH(START_DATE), CAR_ID) AS RECORDS
-        , COUNT(*) OVER(PARTITION BY CAR_ID) AS FILTER
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31'
-   ORDER BY 1, 2 DESC) a
-  WHERE filter >= 5
+      ,car_id 
+      ,records
+  FROM (SELECT DISTINCT MONTH(start_date) month
+               ,car_id
+               ,COUNT(*) OVER(PARTITION BY car_id, MONTH(start_date)) records
+               ,COUNT(*) OVER(PARTITION BY car_id) all_records
+           FROM car_rental_company_rental_history
+          WHERE start_date BETWEEN '2022-08-01' AND '2022-10-31') a
+ WHERE all_records >= 5
+ ORDER BY month, car_id DESC
+
+# SELECT month
+#      ,car_id
+#      ,records
+#   FROM 
+# (SELECT DISTINCT MONTH(START_DATE) AS MONTH
+#         , CAR_ID
+#         , COUNT(*) OVER(PARTITION BY MONTH(START_DATE), CAR_ID) AS RECORDS
+#         , COUNT(*) OVER(PARTITION BY CAR_ID) AS FILTER
+#     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#     WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31'
+#    ORDER BY 1, 2 DESC) a
+#   WHERE filter >= 5
+
+ 
