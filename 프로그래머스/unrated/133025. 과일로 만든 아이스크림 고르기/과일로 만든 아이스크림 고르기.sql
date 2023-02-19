2b4411d60a3bca258e29b123a51b5dfb4fcd93bb
wonil <80016735+wonillin@users.noreply.github.com>
2023-02-20 00:02:39 +0900

    [unrated] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
 SELECT f.flavor
   FROM first_half f
-       INNER JOIN icecream_info i ON f.flavor = i.flavor
+       LEFT JOIN icecream_info i ON f.flavor = i.flavor
  WHERE total_order > 3000
-   AND ingredient_type REGEXP 'fruit'
+   AND ingredient_type LIKE 'fruit%'
+ ORDER BY total_order DESC
