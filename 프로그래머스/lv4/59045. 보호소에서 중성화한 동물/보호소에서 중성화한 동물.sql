ea02a701c1ac6a0caa7febfd78ec34057eb3ea45
wonil <80016735+wonillin@users.noreply.github.com>
2023-02-23 23:39:00 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,12 +1,9 @@
 -- 코드를 입력하세요
-SELECT animal_id
-      ,animal_type
-      ,name
-  FROM animal_ins
- WHERE sex_upon_intake REGEXP 'Intact'
-   AND animal_id IN (SELECT animal_id
-                       FROM animal_outs
-                      WHERE sex_upon_outcome REGEXP ('Spayed|Neutered'))
- ORDER BY animal_id
-                      
-                      
+SELECT i.animal_id
+      ,i.animal_type
+      ,i.name
+  FROM animal_ins i
+       LEFT JOIN animal_outs o ON i.animal_id = o.animal_id
+ WHERE i.sex_upon_intake REGEXP 'intact'
+   AND o.sex_upon_outcome REGEXP 'spayed|neutered'
+ ORDER BY i.animal_id
