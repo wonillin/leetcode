d1e20b07ca317d356ceae9fb138eebb983fdb479
wonil <80016735+wonillin@users.noreply.github.com>
2023-03-22 16:12:31 +0900

    Delete 1158-market-analysis-i directory

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-SELECT user_id 'buyer_id'
-      ,join_date
-      ,SUM(IF(YEAR(order_date) = '2019', 1, 0)) 'orders_in_2019'
-  FROM Users u 
-       LEFT JOIN Orders o ON u.user_id = o.buyer_id
- GROUP BY user_id
- ORDER BY user_id
