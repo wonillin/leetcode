b23fab79e4622cd73b8c05c5a9e90d1f6b3d7f75
wonil <80016735+wonillin@users.noreply.github.com>
2023-03-23 16:27:34 +0900

    Delete 0180-consecutive-numbers directory

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-SELECT DISTINCT l1.num ConsecutiveNums
-  FROM logs l1
-       INNER JOIN logs l2 ON l1.id + 1 = l2.id 
-       INNER JOIN logs l3 ON l2.id + 1 = l3.id 
- WHERE l1.num = l2.num
-   AND l2.num = l3.num
