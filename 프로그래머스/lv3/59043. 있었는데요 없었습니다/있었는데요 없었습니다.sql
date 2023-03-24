753727a8c91614d1a7c248c1945d91dcbcc65ff3
wonil <80016735+wonillin@users.noreply.github.com>
2023-03-24 22:00:40 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 SELECT i.animal_id
       ,i.name
-  FROM animal_ins i, animal_outs o
- WHERE i.animal_id = o.animal_id
-   AND i.datetime > o.datetime
- ORDER BY i.datetime
+  FROM animal_ins i
+       INNER JOIN animal_outs o ON i.animal_id = o.animal_id
+ WHERE i.datetime > o.datetime
+ ORDER BY i.datetime 
