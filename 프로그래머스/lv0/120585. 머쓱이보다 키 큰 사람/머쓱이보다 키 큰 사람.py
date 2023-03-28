d90b59c8ccd41da167dfcf7ba57ebe707c44f49c
wonil <80016735+wonillin@users.noreply.github.com>
2023-03-28 14:43:21 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -0,0 +1,14 @@
+def solution(array, height):
+    count = 0
+    
+    for i in array :     
+        if i > height:
+            count += 1
+    
+    return count
+    
+
+            
+    
+ 
+
