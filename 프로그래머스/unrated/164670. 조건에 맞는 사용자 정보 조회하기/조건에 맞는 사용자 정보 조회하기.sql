d2bec940b9d65068862a8ebba3e30430b0045a8c
wonil <80016735+wonillin@users.noreply.github.com>
2023-05-05 10:41:00 +0000

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT user_id
+      ,nickname
+      ,CONCAT(city, ' ', street_address1, ' ', street_address2) '전체주소'
+      ,CONCAT(SUBSTR(tlno, 1, 3), '-', SUBSTR(tlno, 4, 4), '-', SUBSTR(tlno, 8, 4)) '전화번호'
+  FROM used_goods_user
+ WHERE user_id IN (SELECT writer_id
+                     FROM used_goods_board
+                    GROUP BY writer_id
+                    HAVING COUNT(*) >= 3)
+ ORDER BY user_id DESC
+ 
+
+
