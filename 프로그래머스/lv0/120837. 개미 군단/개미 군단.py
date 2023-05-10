26b000463bf2cd4d9e8c14a0ed6a24f38aa745fd
wonil <80016735+wonillin@users.noreply.github.com>
2023-05-10 16:40:59 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
@@ -1,11 +1,9 @@
 def solution(hp): 
     a = hp // 5
-    a1 = hp % 5
     
-    b = a1 // 3
-    b1 = a1 % 3
+    b = (hp % 5) // 3
     
-    c = b1 / 1
+    c = ((hp % 5) % 3) / 1
     
     
     return a + b + c
