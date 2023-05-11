20d7ce088b19af4c0476fa4a9109e1b0ca2b63ab
wonil <80016735+wonillin@users.noreply.github.com>
2023-05-11 15:48:19 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,11 +1,5 @@
 def solution(num_list):
-    answer = 0
-    answer1 = 0
+    even_num = num_list[::2]
+    odd_num = num_list[1::2]
     
-    for i in range(len(num_list)):
-        if i % 2 == 0:
-            answer += num_list[i]
-        else:
-            answer1 += num_list[i]
-    
-    return answer if answer > answer1 else answer1
+    return max(sum(even_num), sum(odd_num))
