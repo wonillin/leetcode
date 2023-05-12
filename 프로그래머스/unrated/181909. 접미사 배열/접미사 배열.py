cfc69285360a53793da1691b55fcae05f33b9638
wonil <80016735+wonillin@users.noreply.github.com>
2023-05-12 16:16:00 +0900

    [unrated] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
@@ -2,6 +2,6 @@ def solution(my_string):
     answer = []
     
     for i in range(len(my_string)):
-        answer.append(my_string[-i-1:])
+        answer.append(my_string[-i:])
     
     return sorted(answer)
