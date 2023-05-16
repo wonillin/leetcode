21ae8359a3e467daa48058f42e76648ef7f5ca4a
wonil <80016735+wonillin@users.noreply.github.com>
2023-05-16 15:55:43 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,14 +1,13 @@
 -- 코드를 입력하세요
-SELECT a.apnt_no
-      ,p.pt_name
+SELECT apnt_no
+      ,pt_name
       ,p.pt_no
       ,a.mcdp_cd
-      ,d.dr_name
-      ,a.apnt_ymd
+      ,dr_name
+      ,apnt_ymd
   FROM patient p 
-       INNER JOIN appointment a ON p.pt_no = a.pt_no 
-       INNER JOIN doctor d ON a.mddr_id = d.dr_id
- WHERE DATE_FORMAT(a.apnt_ymd, '%Y-%m-%d') = '2022-04-13'  
-   AND a.apnt_cncl_yn REGEXP 'n'
-   AND a.mcdp_cd REGEXP 'cs'
- ORDER BY apnt_ymd
+       LEFT JOIN appointment a ON p.pt_no = a.pt_no
+       LEFT JOIN doctor d ON a.mddr_id = d.dr_id
+ WHERE apnt_ymd BETWEEN '2022-04-13 00:00:00' AND '2022-04-13 23:59:59' 
+   AND apnt_cncl_yn = 'N'
+ ORDER BY apnt_ymd 
