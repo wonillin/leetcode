6ad78ceb1e8feb60ff171651b62e1123b75f12f9
wonil <80016735+wonillin@users.noreply.github.com>
2023-06-05 01:23:03 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,9 @@
+def solution(arr, delete_list):
+    answer = []
+    
+    for i in range(len(arr)):
+        if arr[i] not in delete_list:
+            answer.append(arr[i])
+            
+    
+    return answer
