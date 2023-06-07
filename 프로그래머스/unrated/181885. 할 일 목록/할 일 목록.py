b054ea7712d342de21d01d2fbe85de16be536ca4
wonil <80016735+wonillin@users.noreply.github.com>
2023-06-07 11:57:20 +0900

    [unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,9 @@
+def solution(todo_list, finished):
+    answer = []
+    
+    for t, f in zip(todo_list, finished):
+        if f == False:
+            answer.append(t)
+        
+    
+    return answer
