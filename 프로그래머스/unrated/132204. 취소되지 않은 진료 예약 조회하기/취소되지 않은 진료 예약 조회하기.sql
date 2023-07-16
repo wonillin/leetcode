ff1cb302b68ca7fd97a714ba6de5ba70deefdbcd
wonil <80016735+wonillin@users.noreply.github.com>
2023-07-16 10:41:00 +0000

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -10,4 +10,4 @@ SELECT apnt_no
        LEFT JOIN doctor d ON a.mddr_id = d.dr_id
  WHERE apnt_ymd BETWEEN '2022-04-13 00:00:00' AND '2022-04-13 23:59:59' 
    AND apnt_cncl_yn = 'N'
- ORDER BY apnt_ymd 
+ ORDER BY apnt_ymd
