88baf71798cc8fb4a95e35b4da297304862169a4
wonil <80016735+wonillin@users.noreply.github.com>
2023-07-29 19:00:07 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,6 +1,6 @@
 SELECT cart_id
   FROM cart_products
  GROUP BY cart_id
- HAVING GROUP_CONCAT(name) REGEXP 'milk'
-    AND GROUP_CONCAT(name) REGEXP 'yogurt'
+ HAVING GROUP_CONCAT(name) REGEXP 'yogurt'
+    AND GROUP_CONCAT(name) REGEXP 'milk'
  ORDER BY cart_id
