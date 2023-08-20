b83e671e2dfc4bcc1a0ac1d9e59889a18b7b71ed
wonil <80016735+wonillin@users.noreply.github.com>
2023-08-21 00:07:06 +0900

    Time: 1275 ms (11.80%), Space: 0B (100.00%) - LeetHub

--- 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
+++ 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-SELECT COUNT(customer_id) rich_count
-  FROM 
-(SELECT  customer_id
-       ,SUM(IF(amount > 500, 1, 0)) cnt
+SELECT COUNT(DISTINCT customer_id) rich_count
   FROM store
- GROUP BY customer_id) a
-WHERE cnt >= 1
+ WHERE amount > 500
