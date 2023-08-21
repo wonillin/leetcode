a206f494071d3ebdaaf432747a732c83a820a0dd
wonil <80016735+wonillin@users.noreply.github.com>
2023-08-21 16:16:53 +0900

    [unrated] Title: 특정 옵션이 포함된 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ 프로그래머스/unrated/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT *
-  FROM car_rental_company_car
- WHERE options REGEXP '네비'
- ORDER BY car_id DESC
+select *
+  from car_rental_company_car
+ where options regexp '네비'
+ order by car_id desc
