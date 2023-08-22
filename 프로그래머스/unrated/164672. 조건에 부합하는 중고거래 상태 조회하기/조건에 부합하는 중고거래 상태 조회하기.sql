4f76b8870ae36f1654a9478799b9147e1261d3b2
wonil <80016735+wonillin@users.noreply.github.com>
2023-08-22 11:31:11 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -3,7 +3,10 @@ SELECT board_id
       ,writer_id
       ,title
       ,price
-      ,IF(status = 'DONE', '거래완료', IF(status = 'SALE', '판매중', '예약중')) status
+      ,CASE WHEN status = 'DONE' THEN '거래완료'
+            WHEN status = 'SALE' THEN '판매중'
+            ELSE '예약중' 
+        END status 
   FROM used_goods_board
  WHERE created_date = '2022-10-05'
  ORDER BY board_id DESC
