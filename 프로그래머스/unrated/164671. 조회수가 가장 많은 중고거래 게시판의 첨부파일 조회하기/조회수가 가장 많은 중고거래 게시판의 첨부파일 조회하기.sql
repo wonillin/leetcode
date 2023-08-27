614ca7d714f1410242e7e393bcacef548210a423
wonil <80016735+wonillin@users.noreply.github.com>
2023-08-28 00:40:08 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -3,6 +3,8 @@ SELECT CONCAT('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) fi
   FROM used_goods_file
  WHERE board_id IN (SELECT board_id
                       FROM used_goods_board
-                     WHERE views = (SELECT MAX(views)
-                                     FROM used_goods_board))
+                     WHERE views = (SELECT views
+                                     FROM used_goods_board
+                                    ORDER BY views DESC
+                                    LIMIT 1))
  ORDER BY file_id DESC
