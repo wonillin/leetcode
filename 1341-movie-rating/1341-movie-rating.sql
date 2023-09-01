0c2fe92b7f22c496ebf372878c58f36fd851f77d
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-02 01:18:37 +0900

    Time: 2706 ms (66.78%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,19 +1,18 @@
 # Write your MySQL query statement below
 (SELECT name results
-  FROM movierating m
-       INNER JOIN users u ON m.user_id = u.user_id
+  FROM users u
+       INNER JOIN movierating m ON u.user_id = m.user_id
  GROUP BY name
- ORDER BY COUNT(name) DESC, name
+ ORDER BY COUNT(*) DESC, name
  LIMIT 1)
 
 UNION ALL
 
 (SELECT title results
-  FROM movierating m
-       INNER JOIN movies ms ON m.movie_id = ms.movie_id
+  FROM movies m
+       INNER JOIN movierating r ON m.movie_id = r.movie_id
  WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
  GROUP BY title
  ORDER BY AVG(rating) DESC, title
  LIMIT 1)
-
 
