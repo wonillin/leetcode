f0a719db8c9f9191b5663c0eaa999150e2bd842a
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-05 00:02:29 +0900

    [unrated] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,20 +1,26 @@
--- 코드를 입력하세요
 WITH cte AS
-(SELECT DISTINCT cc.car_id
-      ,cc.car_type
-      ,daily_fee
-      ,CASE WHEN duration_type = '30일 이상' THEN ((1 - (discount_rate * 0.01)) * daily_fee) * LEFT(duration_type, 2) END fee
-  FROM car_rental_company_car cc
-       LEFT JOIN car_rental_company_rental_history rh ON cc.car_id = rh.car_id
-       LEFT JOIN car_rental_company_discount_plan dp ON cc.car_type = dp.car_type
- WHERE cc.car_id NOT IN (SELECT car_id
-                           FROM car_rental_company_rental_history
-                          WHERE '2022-11' BETWEEN DATE_FORMAT(start_date, '%Y-%m') AND DATE_FORMAT(end_date, '%Y-%m'))
-   AND cc.car_type REGEXP '세단|suv')          
+(SELECT DISTINCT car_id
+       ,daily_fee
+       ,car_type
+  FROM car_rental_company_car
+ WHERE car_id NOT IN (SELECT DISTINCT car_id
+                        FROM car_rental_company_rental_history crh
+                       WHERE '2022-11' BETWEEN DATE_FORMAT(start_date, '%Y-%m') AND DATE_FORMAT(end_date, '%Y-%m')
+                     )
+   AND car_type REGEXP '세단|SUV')
+   
+   
 
-SELECT car_id
-      ,car_type
-      ,ROUND(fee) fee
+   
+   
+   
+SELECT cte.car_id
+      ,cdp.car_type
+      ,ROUND((1 - (discount_rate * 0.01))  * (daily_fee * 30)) fee
   FROM cte
- WHERE (fee >= 500000 AND fee < 2000000)
+       LEFT JOIN car_rental_company_discount_plan cdp ON cte.car_type = cdp.car_type
+ WHERE LEFT(duration_type, 2) = 30
+ GROUP BY 1, 2
+ HAVING fee >= 500000
+    AND fee < 2000000
  ORDER BY fee DESC, car_type, car_id DESC
