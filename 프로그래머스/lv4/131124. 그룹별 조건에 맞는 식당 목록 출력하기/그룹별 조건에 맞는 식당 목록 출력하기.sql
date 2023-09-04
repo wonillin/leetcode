c5ae4ee5c54d13467faaa34fb771e93032e5fd41
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-04 23:05:34 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,19 +1,19 @@
-WITH cte AS(
- SELECT member_id
-       ,DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) dr
-   FROM rest_review
-  GROUP BY member_id
-)
+# REVIEW_DATE의 데이트 포맷이 예시와 동일해야 정답처리 됩니다.
+# 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요. 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고, 결과는 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
+WITH rank_member AS
+(SELECT member_id
+      ,DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) dr
+  FROM rest_review
+ GROUP BY member_id)
+
 
 
 SELECT member_name
       ,review_text
       ,DATE_FORMAT(review_date, '%Y-%m-%d') review_date
-  FROM member_profile m
-       INNER JOIN rest_review r ON m.member_id = r.member_id
- WHERE m.member_id IN (SELECT member_id
-                         FROM cte
-                        WHERE dr = 1)
+  FROM member_profile mp
+       INNER JOIN rest_review rr ON mp.member_id = rr.member_id
+ WHERE mp.member_id IN (SELECT member_id
+                          FROM rank_member
+                         WHERE dr = 1)
  ORDER BY review_date, review_text
- 
-     
