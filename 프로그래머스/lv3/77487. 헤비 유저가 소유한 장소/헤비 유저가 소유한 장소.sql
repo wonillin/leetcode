135e447306c35023bc8cb4916526e3d3a8595e0d
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-04 17:36:41 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,14 +1,9 @@
-WITH cte AS(
- SELECT host_id
-       ,COUNT(*) cnt
-   FROM places 
- GROUP BY host_id
- HAVING cnt >= 2
-)
-
-
 SELECT *
-  FROM places
- WHERE host_id IN (SELECT DISTINCT host_id
-                     FROM cte)
+  FROM places p
+ WHERE host_id IN (SELECT host_id
+                     FROM places
+                    GROUP BY host_id
+                    HAVING COUNT(*) > 1)
  ORDER BY id
+
+               
