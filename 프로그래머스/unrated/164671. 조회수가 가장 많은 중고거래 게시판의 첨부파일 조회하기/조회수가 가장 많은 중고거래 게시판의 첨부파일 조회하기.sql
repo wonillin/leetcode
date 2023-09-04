386b98b2d32e9d91cb97f7b9ae2c8ad19931630b
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-04 17:46:27 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,10 +1,15 @@
--- 코드를 입력하세요
+# 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회하는 SQL문을 작성해주세요. 
+# 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬해주세요.
+WITH cte AS(
+ SELECT board_id
+   FROM used_goods_board
+  ORDER BY views DESC
+  LIMIT 1
+)
+
+
 SELECT CONCAT('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) file_path
   FROM used_goods_file
- WHERE board_id IN (SELECT board_id
-                      FROM used_goods_board
-                     WHERE views = (SELECT views
-                                     FROM used_goods_board
-                                    ORDER BY views DESC
-                                    LIMIT 1))
+ WHERE board_id IN (SELECT *
+                      FROM cte)
  ORDER BY file_id DESC
