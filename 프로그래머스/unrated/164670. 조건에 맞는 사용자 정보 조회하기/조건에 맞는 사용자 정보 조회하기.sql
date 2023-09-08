c1005b080d7cd43e95a8ebe9f68ea4b376af033c
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-09 00:50:40 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,8 +1,8 @@
--- 코드를 입력하세요
+-- 중고 거래 3건 등록 / 회원 ID DESC
 SELECT user_id
       ,nickname
       ,CONCAT(city, ' ', street_address1, ' ', street_address2) '전체주소'
-      ,CONCAT(SUBSTR(tlno, 1, 3), '-', SUBSTR(tlno, 4, 4), '-', SUBSTR(tlno, 8, 4)) '전화번호'
+      ,CONCAT(SUBSTRING(tlno, 1, 3), '-', SUBSTRING(tlno, 4, 4), '-', SUBSTRING(tlno, 8, 4)) tlno
   FROM used_goods_user
  WHERE user_id IN (SELECT writer_id
                      FROM used_goods_board
