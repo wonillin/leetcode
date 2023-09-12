d42078df46e96e4e7c5b8f75e98340d470b0d9b5
wonil <80016735+wonillin@users.noreply.github.com>
2023-09-12 16:33:59 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,14 +1,11 @@
-WITH RECURSIVE cte AS(
-    SELECT 0 hour
-    UNION ALL
-    SELECT hour + 1 
-      FROM cte
-     WHERE hour < 23
+WITH RECURSIVE j_hour AS(
+SELECT 0 AS hour
+  UNION ALL
+SELECT hour + 1 FROM j_hour WHERE hour < 23
 )
 
 SELECT hour
-      ,COUNT(hour(ao.datetime)) AS count
-  FROM animal_outs ao
-       RIGHT JOIN cte ON hour(ao.datetime) = hour 
+      ,COUNT(HOUR(ao.datetime)) count
+  FROM j_hour 
+       LEFT JOIN animal_outs ao ON j_hour.hour = HOUR(ao.datetime)
  GROUP BY hour
- ORDER BY hour
