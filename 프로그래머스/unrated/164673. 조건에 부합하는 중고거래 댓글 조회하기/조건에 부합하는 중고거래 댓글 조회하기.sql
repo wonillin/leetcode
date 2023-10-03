962683b4d7852c0f1fdafddcb254a1564c4c9106
wonil <80016735+wonillin@users.noreply.github.com>
2023-10-03 22:43:25 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -4,7 +4,7 @@ SELECT title
       ,reply_id
       ,r.writer_id
       ,r.contents
-      ,DATE_FORMAT(r.created_date, '%Y-%m-%d') AS created_date
+      ,DATE_FORMAT(r.created_date, '%Y-%m-%d') created_date
   FROM used_goods_board b
        INNER JOIN used_goods_reply r ON b.board_id = r.board_id
  WHERE b.created_date BETWEEN '2022-10-01' AND '2022-10-31'
