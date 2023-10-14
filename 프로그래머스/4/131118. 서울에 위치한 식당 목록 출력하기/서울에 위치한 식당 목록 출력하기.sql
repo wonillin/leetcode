cf3a1d833de41ba0c47c3bd510095e8331163756
wonil <80016735+wonillin@users.noreply.github.com>
2023-10-15 01:30:48 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,11 @@
+SELECT i.rest_id
+      ,rest_name
+      ,food_type
+      ,favorites
+      ,address
+      ,ROUND(AVG(review_score), 2) score
+  FROM rest_info i 
+       INNER JOIN rest_review r ON i.rest_id = r.rest_id
+ WHERE address REGEXP '^서울'
+ GROUP BY rest_id
+ ORDER BY score DESC, favorites DESC
